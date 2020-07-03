47b26fc42cd92c5c72447373f576a35519a37798
Kesav Kosana <kkosana33@gmail.com>
2020-07-03 02:47:08 -0400

    removing warning messages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { firestore } from './firebase/firebase.utils';
+// import { firestore } from './firebase/firebase.utils';
 
 import './App.css';
 
